2501e8f21d3a161784549799f589c7456fb14364
mitermayer18 <jardelska@gmail.com>
2021-04-16 02:38:45 -0300

    feat:criaçao e estilizacao do Menu

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,14 +1,30 @@
 import React from 'react';
 import {
+  SafeAreaView,
   View,
-  Text
+  Text,
+  StyleSheet
 } from 'react-native';
+
+import Menu from '../../components/Menu';
+import Header from '../../components/Header';
 
 export default function Home(){
   return(
-    <View>
-      <Text>Pagina Home</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Menu />
+      <Header />
+    </SafeAreaView>
   )
 
 }
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    justifyContent:'center',
+    alignItems:'center',
+    backgroundColor:'#E8F0FF',
+    paddingTop: '5%'
+  }
+})
